e73b1338c91ede39ff5084303d7f8fe5aa241b80
Flovoko <79543418+Flovoko@users.noreply.github.com>
2022-04-05 13:16:29 +0200

    Delete Minecraft-Server-Tools W.I.P directory

--- Minecraft-Server-Tools W.I.P/WIP Mc Server Download & Install scrip for Windows.bat
+++
@@ -1,9 +0,0 @@
-title Minecraft Server Install Script
-@echo off
-echo Minecraft Server Install Script (Download Mirror Minecraft.net;Archive McVersions.net)
-pause
-
-cd Resources
-mkdir Server
-cd Versions
-./version-s.bat

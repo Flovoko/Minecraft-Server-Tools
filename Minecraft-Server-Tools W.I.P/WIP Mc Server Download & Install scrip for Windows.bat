0706e4c1fc80350bfb98a60ebe11960f5c61f7dd
Flovoko <79543418+Flovoko@users.noreply.github.com>
2022-04-05 12:58:13 +0200

    Add files via upload

---
+++ Minecraft-Server-Tools W.I.P/WIP Mc Server Download & Install scrip for Windows.bat
@@ -0,0 +1,9 @@
+title Minecraft Server Install Script
+@echo off
+echo Minecraft Server Install Script (Download Mirror Minecraft.net;Archive McVersions.net)
+pause
+
+cd Resources
+mkdir Server
+cd Versions
+./version-s.bat
